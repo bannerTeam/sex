3b40dee3d0076478134b1630b5f2c82a52c8da54
gary <garry_md_233@ie8.com>
2018-10-19 13:15:49 +0800

    增加 99l 模板首页广告

--- application/extra/maccms.php
+++ application/extra/maccms.php
@@ -26,10 +26,10 @@ return array (
     'site_qq' => '2640963460',
     'site_email' => '2640963460@qq.com',
     'install_dir' => '/',
-    'template_dir' => 'avhub',
+    'template_dir' => '99a11',
     'html_dir' => 'html',
     'mob_status' => '0',
-    'mob_template_dir' => 'avhub',
+    'mob_template_dir' => '99a11',
     'mob_html_dir' => 'html',
     'site_tj' => 'var _hmt = _hmt || [];
 	(function() {
